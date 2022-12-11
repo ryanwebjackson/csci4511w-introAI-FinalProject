bc00e25bc54e2274a8c5f209d5569c80915a36d4
Ryan Jackson <ryanjackson0y-@live.com>
2022-12-11 11:07:42 -0600

    Create lazy_animal_random_agent_problem.py

---
+++ lazy_animal_random_agent_problem.py
@@ -0,0 +1,1 @@
+# Problem/Game definition, where both agents have random activity - this will serve, in combination with average(s) measurement, as a control (group) for this group of experiments (project).
