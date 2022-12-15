a9ac57a4d9bf868bc300f38e97bc8fca3926bae5
Ryan Jackson <ryanjackson0y-@live.com>
2022-12-15 15:56:31 -0600

    WIP

--- runner_genetic_game1.py
+++ runner_genetic_game1.py
@@ -6,11 +6,10 @@ from tests.test_search import *  # type: ignore
 
 class RunnerGeneticGame1:
 
-    # def genetic_search(problem, ngen=1000, pmut=0.1, n=20):
-    # use this function from search.py, and pass in the problem
-
     @staticmethod
     def run_genetic():
         # See AIMA code example: test_genetic_algorithm
         print("inside run_genetic")
-        pass
+
+        # def genetic_search(problem, ngen=1000, pmut=0.1, n=20):
+        # use this function from search.py, and pass in the problem
