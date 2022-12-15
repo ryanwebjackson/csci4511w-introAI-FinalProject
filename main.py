83b826fe145f47040197942ae88fc511c66d7900
Ryan Jackson <ryanjackson0y-@live.com>
2022-12-15 12:15:11 -0600

    WIP: Add example code for GA, command line (terminal) option.
    TODO: Move specific algo runner code out, and Start GA implementation. Consider interactive user command line options.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
-
-from lazy_animal_state import LazyAnimalState
-
 sys.path.append('aima-python')
 
+from lazy_animal_state import LazyAnimalState
+from search import *  # type: ignore
+from tests.test_search import *  # type: ignore
 from games import alpha_beta_search  # type: ignore
 from lazy_animal_minimax_game1 import LazyAnimalMinimaxGame1
 
@@ -16,8 +16,7 @@ class MainHelper:
         pass
 
 
-def main():
-
+def run_minimax():
     game = LazyAnimalMinimaxGame1()
 
     initial_state = LazyAnimalState()
@@ -35,5 +34,67 @@ def main():
     # Ideal: Return the whole path, from initial state to terminal state (and if goal was reached). jack1805
 
 
+def run_genetic():
+    # AIMA code example for bootstrapping:
+
+    def aimatest_genetic_algorithm():
+        # Graph coloring
+        edges = {'A': [0, 1],
+                 'B': [0, 3],
+                 'C': [1, 2],
+                 'D': [2, 3]}
+
+        def fitness(c):
+            return sum(c[n1] != c[n2] for (n1, n2) in edges.values())
+
+        solution_chars = GA_GraphColoringChars(edges, fitness)
+        assert solution_chars == ['R', 'G', 'R', 'G'] or solution_chars == ['G', 'R', 'G', 'R']
+
+        solution_bools = GA_GraphColoringBools(edges, fitness)
+        assert solution_bools == [True, False, True, False] or solution_bools == [False, True, False, True]
+
+        solution_ints = GA_GraphColoringInts(edges, fitness)
+        assert solution_ints == [0, 1, 0, 1] or solution_ints == [1, 0, 1, 0]
+
+        # Queens Problem
+        gene_pool = range(8)
+        population = init_population(100, gene_pool, 8)
+
+        def fitness(q):
+            non_attacking = 0
+            for row1 in range(len(q)):
+                for row2 in range(row1 + 1, len(q)):
+                    col1 = int(q[row1])
+                    col2 = int(q[row2])
+                    row_diff = row1 - row2
+                    col_diff = col1 - col2
+
+                    if col1 != col2 and row_diff != col_diff and row_diff != -col_diff:
+                        non_attacking += 1
+
+            return non_attacking
+
+        solution = genetic_algorithm(population, fitness, gene_pool=gene_pool, f_thres=25)
+        print("fitness(solution) >= 25: ", fitness(solution) >= 25)
+
+    aimatest_genetic_algorithm()
+
+
+def main(args):
+
+    if len(args) == 0:
+        print('No arguments passed.')
+        return
+
+    # jack1805: This command structure works, but is not obvious,
+    # if the user doesn't have access to the code (i.e. not user friendly).
+    if args[0] == "minimax":
+        print("running minimax (alpha-beta pruning)...")
+        run_minimax()
+    elif args[0] == "genetic":
+        print("running genetic algorithm...")
+        run_genetic()
+
+
 if __name__ == '__main__':
-    main()
+    main(args=sys.argv[1:])
