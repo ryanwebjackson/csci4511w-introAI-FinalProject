94e0b4d47a3bb61de3ad5428d4a36bad35c1449f
Ryan Jackson <ryanjackson0y-@live.com>
2022-12-15 17:37:05 -0600

    WIP: Get GA working.

--- lazy_animal_genetic_problem.py
+++ lazy_animal_genetic_problem.py
@@ -1,5 +1,5 @@
 # Planned on doing 2 different forms of GA for this project, but get one working first (by any reasonable means).
-from typing import List
+from typing import List, Optional
 
 from lazy_animal_constants import Constants
 from lazy_animal_state import LazyAnimalState
@@ -15,6 +15,7 @@ class LazyAnimalGeneticProblem(Problem):
         self.successors = successors
         self.utils = utils
         self.initial = initial
+        self.initial_state = initial  # Make the genetic_search() function happy.
         # Can have a Goal state if unique - specified by parent class Problem.
         self.goal = None
 
@@ -25,13 +26,44 @@ class LazyAnimalGeneticProblem(Problem):
     def actions(self, state: LazyAnimalState) -> List[str]:
         return list(self.successors.get(state.tree_key, {}).keys())
 
-    def result(self, state: LazyAnimalState, action) -> LazyAnimalState:
+    def result(self, state: LazyAnimalState, action) -> str:
+        """ Returning the state key instead of object, to work with existing genetic_search function. """
         return self.successors[state.tree_key][action]
 
-    def value(self, state: LazyAnimalState):
+    def value(self, state: LazyAnimalState) -> int:
         """
         fitness function: Takes a list of states,
         and returns a list of fitness values based on some (domain) logic.
         """
+        subject_dict: dict = dict()
+        if state.tree_key in self.utils:
+            subject_dict = self.utils
+        elif state.tree_key in self.successors:
+            subject_dict = self.successors
+        else:
+            raise Exception("state.tree_key (on state passed to value fn) not found in successors or utils (second-level successors).")
+
+        # Get the fitness value for the state passed in.
+        subject_state_val: int = 0
+        if isinstance(subject_dict[state.tree_key], dict):
+            subject_state_val = self.recurse_to_find_subject_state_val(subject_dict[state.tree_key]) or 0
+        else:
+            subject_state_val = subject_dict[state.tree_key]
+
         # "How close are we to a goal state?":
-        return abs(Constants.target_animal_kcalories - self.utils[state.tree_key])
+        return abs(Constants.target_animal_kcalories - subject_state_val)
+
+    def recurse_to_find_subject_state_val(self, subject_dict: dict, subject_state_val: Optional[int] = None) -> Optional[int]:
+        for key, value in subject_dict.items():
+            if isinstance(value, dict):
+                self.recurse_to_find_subject_state_val(value, subject_state_val)
+            elif isinstance(value, int):
+                subject_state_val = value
+                break
+            elif isinstance(value, LazyAnimalState):
+                subject_state_val = value.current_animal_kcalories
+                break
+            else:
+                raise Exception("Unexpected value type in recurse_to_find_subject_state_val. type: ", type(value))
+
+        return subject_state_val
--- runner_genetic_game1.py
+++ runner_genetic_game1.py
@@ -1,6 +1,9 @@
 import sys
+
 sys.path.append('aima-python')
 
+from lazy_animal_state_helper import LazyAnimalStateHelper
+from lazy_animal_genetic_problem import LazyAnimalGeneticProblem
 from tests.test_search import *  # type: ignore
 
 
@@ -8,8 +11,12 @@ class RunnerGeneticGame1:
 
     @staticmethod
     def run_genetic():
-        # See AIMA code example: test_genetic_algorithm
+        # See AIMA code example for a reference of how to run
+        # (what params mean, etc.): test_genetic_algorithm in tests/test_search.py
         print("inside run_genetic")
 
-        # def genetic_search(problem, ngen=1000, pmut=0.1, n=20):
-        # use this function from search.py, and pass in the problem
+        initial = LazyAnimalStateHelper.get_initial_state()
+        successors = LazyAnimalStateHelper.get_successors_for_tree1()
+        utils = LazyAnimalStateHelper.get_terminal_states_for_tree1()
+        problem = LazyAnimalGeneticProblem(initial, successors, utils)
+        genetic_search(problem, ngen=1000, pmut=0.1, n=20)
