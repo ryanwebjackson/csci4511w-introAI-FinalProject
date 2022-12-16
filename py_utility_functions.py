54d46dfb65bb7688e54c7ecfa293d083bb6c97d8
Ryan Jackson <ryanjackson0y-@live.com>
2022-12-16 16:43:03 -0600

    WIP: Start adding profiling (performance analysis) functions

--- py_utility_functions.py
+++ py_utility_functions.py
@@ -7,3 +7,19 @@ class PyUtilityFunctions:
     def print_traceback():
         for line in traceback.format_stack():
             print(line.strip())
+
+    # region Python Program/Script Profiling and Performance Analysis
+
+    def get_current_ram_usage(self):
+        pass
+
+    def get_current_cpu_usage(self):
+        pass
+
+    def get_current_disk_usage(self):
+        pass
+
+    def get_current_time_running(self):
+        pass
+
+    # endregion
